18eb6aef2d7cc67135257f0537cb22105fbdaed8
Julio Berina <julioberina@gmail.com>
2017-05-22 22:00:06 +0000

    Put in smtp settings for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,12 +27,24 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
 
   # Default url options for dev
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
