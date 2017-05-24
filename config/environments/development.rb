69b762b17db5aeb33810c929b6706845ae633688
Julio Berina <julioberina@gmail.com>
2017-05-24 07:22:28 +0000

    Associated place with NotificationMailer templates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
+  # Perform deliveries
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
